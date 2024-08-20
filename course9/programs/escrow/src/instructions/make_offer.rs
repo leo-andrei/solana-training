4d877c7f3cf9bdda77f675540369bc2f854a66c8
Andrei Leonte <aleonte@fluidtrends.com>
2024-08-20 16:53:07 +0300

    add course 10 escrow missing files

---
+++ course9/programs/escrow/src/instructions/make_offer.rs
@@ -0,0 +1,87 @@
+use crate::{Offer, ANCHOR_DISCRIMINATOR};
+use anchor_lang::prelude::*;
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token_interface::{transfer_checked, Mint, TokenAccount, TokenInterface, TransferChecked},
+};
+
+#[derive(Accounts)]
+#[instruction(id: u64)]
+pub struct MakeOffer<'info> {
+    #[account(mut)]
+    pub maker: Signer<'info>,
+
+    #[account(mint::token_program = token_program)]
+    pub token_mint_a: InterfaceAccount<'info, Mint>,
+
+    #[account(mint::token_program = token_program)]
+    pub token_mint_b: InterfaceAccount<'info, Mint>,
+
+    #[account(
+        mut,
+        associated_token::mint = token_mint_a,
+        associated_token::authority = maker,
+        associated_token::token_program = token_program
+    )]
+    pub maker_token_account_a: InterfaceAccount<'info, TokenAccount>,
+
+    #[account(
+        init,
+        payer = maker,
+        space = ANCHOR_DISCRIMINATOR + Offer::INIT_SPACE,
+        seeds = [b"offer", maker.key().as_ref(), id.to_le_bytes().as_ref()],
+        bump
+    )]
+    pub offer: Account<'info, Offer>,
+
+    #[account(
+        init,
+        payer = maker,
+        associated_token::mint = token_mint_a,
+        associated_token::authority = offer,
+        associated_token::token_program = token_program
+    )]
+    pub vault: InterfaceAccount<'info, TokenAccount>,
+
+    pub associated_token_program: Program<'info, AssociatedToken>,
+
+    pub token_program: Interface<'info, TokenInterface>,
+
+    pub system_program: Program<'info, System>,
+}
+
+pub fn send_offered_tokens_to_vault(
+    ctx: &Context<MakeOffer>,
+    token_a_offered_amount: u64,
+) -> Result<()> {
+    let transfer_accounts = TransferChecked {
+        from: ctx.accounts.maker_token_account_a.to_account_info(),
+        mint: ctx.accounts.token_mint_a.to_account_info(),
+        to: ctx.accounts.vault.to_account_info(),
+        authority: ctx.accounts.maker.to_account_info(),
+    };
+
+    let cpi_ctx = CpiContext::new(
+        ctx.accounts.token_program.to_account_info(),
+        transfer_accounts,
+    );
+
+    transfer_checked(
+        cpi_ctx,
+        token_a_offered_amount,
+        ctx.accounts.token_mint_a.decimals,
+    )
+}
+
+pub fn save_offer(ctx: Context<MakeOffer>, id: u64, token_b_wanted_amount: u64) -> Result<()> {
+    ctx.accounts.offer.set_inner(Offer {
+        id,
+        maker: ctx.accounts.maker.key(),
+        token_mint_a: ctx.accounts.token_mint_a.key(),
+        token_mint_b: ctx.accounts.token_mint_b.key(),
+        token_b_wanted_amount,
+        bump: ctx.bumps.offer,
+    });
+
+    Ok(())
+}
--- course9/programs/escrow/src/lib.rs
+++ course9/programs/escrow/src/lib.rs
@@ -18,4 +18,21 @@ pub mod escrow {
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         initialize::handler(ctx)
     }
+
+    pub fn make_offer(
+        ctx: Context<MakeOffer>,
+        id: u64,
+        token_a_offered_amount: u64,
+        token_b_wanted_amount: u64,
+    ) -> Result<()> {
+        instructions::make_offer::send_offered_tokens_to_vault(&ctx, token_a_offered_amount)?;
+
+        instructions::make_offer::save_offer(ctx, id, token_b_wanted_amount)
+    }
+
+    pub fn take_offer(context: Context<TakeOffer>) -> Result<()> {
+        instructions::take_offer::send_wanted_tokens_to_maker(&context)?;
+
+        instructions::take_offer::withdraw_and_close_vault(context)
+    }
 }
